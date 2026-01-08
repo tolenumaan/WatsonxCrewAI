f909f5066ffbf839e28cb069f41407ec83c3a528
tolenumaan <tolenumaan@gmail.com>
2026-01-08 22:09:16 +0530

    Update Watsonx project ID in agent.py

--- agent.py
+++ agent.py
@@ -14,7 +14,7 @@ llm = WatsonxLLM(
     model_id="meta-llama/llama-3-70b-instruct",
     url="https://us-south.ml.cloud.ibm.com",
     params=parameters,
-    project_id="<YOUR WATSONX.AI PROJECT ID HERE>",
+    project_id="160bb1c8-24f9-4380-b051-77fa2d300aaf",
 )
 
 # Create the function calling llm
